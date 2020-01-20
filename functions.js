623e3a92ea2cb82e5dee51cfbd694d9418786d6b
Pauline P. Narvas <paulinenarvas@gmail.com>
2020-01-20 15:13:47 +0000

    Add js

---
+++ functions.js
@@ -0,0 +1,24 @@
+function myFunction() {
+  var x = document.getElementById("inspiringfigures");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} 
+function myFunction1() {
+  var x = document.getElementById("resources");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} 
+function myFunction2() {
+  var x = document.getElementById("services");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} 
